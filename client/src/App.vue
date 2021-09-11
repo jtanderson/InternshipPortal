a4fc49f808ff91ba412d11b13ac4d0e737d1bacf
Jacob <jacobduncan00@yahoo.com>
2021-09-11 18:50:12 -0400

    added vuejs scaffolding

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <HomePageHeader />
+</template>
+
+<script>
+import HomePageHeader from "./components/HomePageHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    HomePageHeader,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ client/src/components/HomePageHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-gray-200 h-screen">
+    <div
+      class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:items-center lg:justify-between"
+    >
+      <h2
+        class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl lg:mb-12"
+      >
+        <span class="block text-indigo-600 text-center"
+          >Salisbury University Internship Web Portal</span
+        >
+      </h2>
+      <div class="mt-8 lg:mt-0 lg:flex-shrink-0">
+        <div class="inline-flex rounded-md shadow">
+          <a
+            href="/signup"
+            class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-white hover:bg-indigo-700 hover:text-white border-6 border-indigo-700"
+          >
+            Sign Up
+          </a>
+        </div>
+        <div class="ml-3 inline-flex rounded-md shadow">
+          <a
+            href="/signin"
+            class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-white hover:bg-indigo-700 hover:text-white border-6 border-indigo-700"
+          >
+            Sign In
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
