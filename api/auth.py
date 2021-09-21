f06e1fbcd392de528800b985868fada340b84052
jventura1738 <justinv9001@aol.com>
2021-09-21 18:20:07 -0400

    Move auth routes to auth.py

--- api/auth.py
+++ api/auth.py
@@ -1,12 +1,10 @@
 # Internship Portal Web App
-# __init__.py for API
+# auth.py for API
 
 
 """
 Clients: Dr. Joseph Anderson, Dr. Giulia Franchi
 Team: Blaine Mason, Jacob Duncan, Justin Ventura, Margaret Finnegan
-
-For now just store API in the __init__.py file, this will change later.
 """
 
 
@@ -18,34 +16,19 @@ from flask import render_template, Blueprint, request, redirect, url_for
 auth = Blueprint('auth', __name__)
 
 # ------------------------------------------------------------------------
-#       ADMIN PAGE ROUTES: these routes are all for admin page.
+#          AUTH ROUTES: these routes are all for authorization
 # ------------------------------------------------------------------------
 
-@auth.route('/login', methods=['GET'])
-def login():
-    """Admin login page.
+
+# Handles the login form post requests:
+# TODO: handle login correctly.
+@auth.route('/login-submit', methods=['POST'])
+def login_submit():
+    """Login submission route.
+    This function handles the login submission attempts.
     """
-    return render_template("login.html", page_title="Admin Login", jinja_data="Hello from Jinja")
-
-
-@auth.route('/login', methods=['POST'])
-def login_post():
-    email = request.POST.get('email')
-    name = request.POST.get('name')
+    username = request.POST.get('username')
     password = request.POST.get('password')
-
-    # IMPLEMENT THE FOLLOWING ONCE WE HAVE DB MODEL:
-
-    # user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
-
-    # if user: # if a user is found, we want to redirect back to signup page so user can try again
-    #     return redirect(url_for('auth.signup'))
-
-    # # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    # new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-
-    # # add the new user to the database
-    # db.session.add(new_user)
-    # db.session.commit()
+    print(f'Username: {username}, Password: {password}')
 
     return redirect(url_for('auth.login'))
