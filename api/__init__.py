f1a783bcfa978504eb832871dde9cfd16d089124
jventura1738 <justinv9001@aol.com>
2021-09-21 18:19:36 -0400

    Move routes out of __init__.py to views.py

--- api/__init__.py
+++ api/__init__.py
@@ -27,35 +27,13 @@ def create_app():
     CORS(app)
     app.config['SECRET_KEY'] = ''
 
+    # Blueprint for views routes in the app:
+    from .views import views as views_blueprint
+    app.register_blueprint(views_blueprint)
+
     # Blueprint for auth routes in the app:
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     # Return the app to be run in main.py:
     return app
-
-
-# ------------------------------------------------------------------------
-#                                 ROUTES
-# ------------------------------------------------------------------------
-
-# Root page route: 
-@app.route('/')
-def root():
-    """Root page view route.
-    This function runs whenever the root page (url below) is requested.
-        ex) -> localhost:5000/
-    Returns the root page: which renders index.html (landing page)
-    """
-    return render_template("index.html", page_title="Internship Web Portal Homepage")
-
-
-# Admin view route:
-@app.route('/admin')
-def admin():
-    """Admin page view route.
-    This function runs whenver the admin page ('/admin') is requested.
-        ex) -> localhost:5000/admin
-    Returns the admin page: which renders admin.html (admin dashboard)
-    """
-    return render_template("admin.html")
