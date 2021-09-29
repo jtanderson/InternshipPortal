538277e7c0bce78623a5f5f706ca561c5fefbf29
jventura1738 <justinv9001@aol.com>
2021-09-29 12:46:38 -0400

    Add template model for user, experimental

---
+++ api/models.py
@@ -0,0 +1,27 @@
+# Justin Ventura
+
+"""
+This module is for the database models.
+"""
+
+# Grab the database object from api.
+from api import db
+
+
+# Users Models Class:
+class UsersModel(db.Model):
+    __tablename__ = 'user_test'
+
+    # Table attributes:
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String())
+    email = db.Column(db.String())
+    password = db.Column(db.String())
+
+    def __init__(self, username: str, email: str, password: str):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return f'<User {self.username}>'
