da9bb4c893d7b27af8797efba4554feb5577c321
jventura1738 <justinv9001@aol.com>
2021-09-30 00:06:57 -0400

    Add functionality for username/password check in correct_login

--- api/helpers.py
+++ api/helpers.py
@@ -4,8 +4,17 @@
 This module contains functions used to help out with the app.
 """
 
+# Database functinality imports
+from api import db
+from .models import UserModel
+
 
 # TODO: check database here.
 # Check credentials against the database:
 def correct_login(username: str, password: str) -> bool:
-    return True
+
+    # Grab the admin from the database:
+    admin = UserModel.query.filter_by(username=username)
+
+    # Pythonic ternary babyyyyy
+    return False if not admin or password != admin.password else True
