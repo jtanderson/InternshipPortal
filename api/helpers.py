ff35d4a5798b02b7e49001414cd720a8da43b029
jventura1738 <justinv9001@aol.com>
2021-09-30 02:00:06 -0400

    Fix method to check if user in user database

--- api/helpers.py
+++ api/helpers.py
@@ -5,7 +5,6 @@ This module contains functions used to help out with the app.
 """
 
 # Database functinality imports
-from api import db
 from .models import UserModel
 
 
@@ -14,7 +13,7 @@ from .models import UserModel
 def correct_login(username: str, password: str) -> bool:
 
     # Grab the admin from the database:
-    admin = UserModel.query.filter_by(username=username)
+    admin = UserModel.query.filter_by(username=username).first()
 
     # Pythonic ternary babyyyyy
     return False if not admin or password != admin.password else True
