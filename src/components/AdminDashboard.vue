1a971e6f3670d992d55368ce4fcb2d4c02c77844
Jacob <jacobduncan00@yahoo.com>
2021-10-02 21:19:29 -0400

    Update UI for Admin Dashboard

--- src/components/AdminDashboard.vue
+++ src/components/AdminDashboard.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="h-screen">
+  <div class="h-screen flex">
     <AdminStats />
+    <AdminCard />
   </div>
 </template>
 
 <script>
 import AdminStats from "./AdminStats.vue";
+import AdminCard from "./AdminCard.vue";
 export default {
   name: "AdminDashboard",
   components: {
     AdminStats,
+    AdminCard,
   },
 };
 </script>
