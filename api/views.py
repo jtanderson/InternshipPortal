c61bf0a5a56e93ed116f92886301dca5b003f58a
jventura1738 <justinv9001@aol.com>
2021-10-04 09:15:37 -0400

    Remove temp route

--- api/views.py
+++ api/views.py
@@ -60,16 +60,3 @@ def contact():
     Returns the login page: which renders contact.html (contact page)
     """
     return render_template('contact.html', page_title='Contact Us')
-
-
-# TODO: Find a better way to seed default data into database.
-@views.route('/temp', methods=['GET'])
-def temp():
-    from api import db, UsersModel
-    me = UsersModel(username='jventura3', 
-                   email='jventura3@gulls.salisbury.edu',
-                   password='a900a10c4d321f8338325b3cea14cbaa53a87bd3462bac578ab3966ea7ab9db8')
-
-    db.session.add(me)
-    db.session.commit()
-    return {'temp': 'test'}, 200
