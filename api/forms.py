c2f2c92ace5228bfbac382e255a846525665f4a8
Blaine Mason <bmason3@gulls.salisbury.edu>
2021-10-04 20:10:17 -0400

    Add form to insert listing

--- api/forms.py
+++ api/forms.py
@@ -28,3 +28,20 @@ def contact_submit():
     print(f'Name: {name}, Email: {email}, Message: {message}')
 
     return 200  # Status code success
+
+# Route for submitting forms:
+@forms.route('/listing-submit', methods=['POST'])
+def listing_submit():
+    """Listing submission route.
+    This function handles the listing submissions.
+    """
+    data = request.json
+    client_id = data['client_id']
+    position = data['position']
+    pos_responsibility = data['pos_responsibility']
+    min_qualifications = data['min_qualifications']
+    pref_qualifications = data['pref_qualifications']
+    additional_info = data['additional_info']
+    print(f'Client ID: {client_id}, Position: {position}, Position Responsibility: {pos_responsibility}, Minimum Qualifications: {min_qualifications}, Preffered Qualifications: {pref_qualifications}')
+
+    return 200  # Status code success
