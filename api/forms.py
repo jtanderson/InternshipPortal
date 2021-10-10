e3340cd822190a0ee6c4b46bfadcc8b5f7aba8db
jventura1738 <justinv9001@aol.com>
2021-10-09 23:48:08 -0400

    Change company to client in form

--- api/forms.py
+++ api/forms.py
@@ -11,6 +11,8 @@ For now just store API in the forms.py file, this will change later.
 
 from flask import Blueprint, request
 
+from .models import ClientsModel, ListingsModel
+
 # Create auth blueprint:
 forms = Blueprint('forms', __name__)
 
@@ -36,17 +38,19 @@ def listing_submit():
     This function handles the listing submissions.
     """
     data = request.json
-    company_name = data["company_name"],
-    company_address = data["company_address"],
-    company_city = data["company_city"],
-    company_state = data["company_state"],
-    company_zip = data["company_zip"],
+    client_name = data["client_name"],
+    client_address = data["client_address"],
+    client_city = data["client_city"],
+    client_state = data["client_state"],
+    client_zip = data["client_zip"],
     position_title = data['position_title']
     pos_responsibility = data['pos_responsibility']
     min_qualifications = data['min_qualifications']
     pref_qualifications = data['pref_qualifications']
     additional_info = data['additional_info']
-    print(f'Company Name: {company_name}, Company Address: {company_address}, Company City: {company_city}, Company State: {company_state}, Company Zip: {company_zip}, Position Title: {position_title}, Position Responsibility: {pos_responsibility}, Minimum Qualifications: {min_qualifications}, Preffered Qualifications: {pref_qualifications}, Additional Info: {additional_info}')
+    # print(f'client Name: {client_name}, client Address: {client_address}, client City: {client_city}, client State: {client_state}, client Zip: {client_zip}, Position Title: {position_title}, Position Responsibility: {pos_responsibility}, Minimum Qualifications: {min_qualifications}, Preffered Qualifications: {pref_qualifications}, Additional Info: {additional_info}')
+
+    client = ClientsModel()
 
     response = {"status": 200}
     return response  # Status code success
