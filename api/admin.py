6fdc542eaef41c3745b76ef4069fcf5977df964c
jventura1738 <justinv9001@aol.com>
2021-10-25 09:32:34 -0400

    Add before-request hook for admin routes

--- api/admin.py
+++ api/admin.py
@@ -19,6 +19,13 @@ admin = Blueprint('admin', __name__, url_prefix='/admin')
 #          ADMIN ROUTES: these routes are all for the admin
 # ------------------------------------------------------------------------
 
+# Ensure that client is in admin session:
+@admin.before_request
+def _admin_session():
+    """Runs before all admin routes to check session."""
+    if admin_session() is False:
+        return {'err_msg': 'ACCESS DENIED.'}, 403
+
 
 # STATUSES: inactive, active, pending, rejected.
 @admin.route('/get-listings/<status>', methods=['GET'])
@@ -47,51 +54,43 @@ def get_listings(status: str = 'all'):
     """
     response = dict()
 
-    # If in an admin session:
-    if admin_session():
+    # Handle possible listing statuses:
+    listings = []
 
-        # Handle possible listing statuses:
-        listings = []
+    # All statuses:
+    if status == 'all':
+        listings = ListingsModel.query.all()
 
-        # All statuses:
-        if status == 'all':
-            listings = ListingsModel.query.all()
+    # Query for one of the four statuses:
+    elif status in LISTING_STATUSES:
+        listings = ListingsModel.query.filter_by(status=status).all()
 
-        # Query for one of the four statuses:
-        elif status in LISTING_STATUSES:
-            listings = ListingsModel.query.filter_by(status=status).all()
-
-        # Invalid listings request:
-        else:
-            response['err_msg'] = 'Invalid listings request.'
-            return response, 400
-
-        # Valid listing request:
-        if listings is not None:
-            code = 200
-            # For all pending listings, create a payload for each one:
-            for i, listing in enumerate(listings):
-
-                # Use foreign key to get client name via client_id:
-                temp = ClientsModel.query.filter_by(id=listing.client_id)\
-                    .first()
-                client_name = temp.client_name
-
-                # Create the payload:
-                response[i] = {
-                    'client': client_name,
-                    'listing': listing.to_dict()
-                }
-
-        # No listings found.
-        else:
-            response['err_msg'] = 'No listings found.'
-            code = 200
-
-    # If NOT in admin session, deny access:
+    # Invalid listings request:
     else:
-        response['err_msg'] = 'ACCESS DENIED.'
-        code = 403
+        response['err_msg'] = 'Invalid listings request.'
+        return response, 400
+
+    # Valid listing request:
+    if listings is not None:
+        code = 200
+        # For all pending listings, create a payload for each one:
+        for i, listing in enumerate(listings):
+
+            # Use foreign key to get client name via client_id:
+            temp = ClientsModel.query.filter_by(id=listing.client_id)\
+                .first()
+            client_name = temp.client_name
+
+            # Create the payload:
+            response[i] = {
+                'client': client_name,
+                'listing': listing.to_dict()
+            }
+
+    # No listings found.
+    else:
+        response['err_msg'] = 'No listings found.'
+        code = 200
 
     return response, code
 
@@ -103,26 +102,20 @@ def action_on_listing(id: int, status: str):
     NOTE: must be in admin session.
     """
     response = dict()
-    # If in an admin session:
-    if admin_session():
 
-        # Valid status:
-        if status in LISTING_STATUSES:
-            listing = ListingsModel.query.get(id)
-            listing.status = status
-            db.session.commit()
+    # Valid status:
+    if status in LISTING_STATUSES:
+        listing = ListingsModel.query.get(id)
+        listing.status = status
+        db.session.commit()
 
-            response['listing'] = listing.to_dict()
-            code = 200
+        response['listing'] = listing.to_dict()
+        code = 200
 
-        # Invalid status:
-        else:
-            response['err_msg'] = 'Invalid status'
-            code = 400
-    # If NOT in admin session, deny access:
+    # Invalid status:
     else:
-        response['err_msg'] = 'Access Denied.'
-        code = 403
+        response['err_msg'] = 'Invalid status'
+        code = 400
 
     return response, code
 
@@ -136,24 +129,18 @@ def star_listing(listing_id: int):
     If a listing is starred, unstar it.
     """
     response = dict()
-    # Is an admin:
-    if admin_session():
 
-        # Check if listing is in database, then update and return to Jake:
-        if listing := ListingsModel.query.filter_by(id=listing_id).first():
-            listing.starred = True if listing.starred is False else False
-            db.session.commit()
-            response['listing'] = listing.to_dict()
-            code = 200
+    # Check if listing is in database, then update and return to Jake:
+    if listing := ListingsModel.query.filter_by(id=listing_id).first():
+        listing.starred = True if listing.starred is False else False
+        db.session.commit()
+        response['listing'] = listing.to_dict()
+        code = 200
 
-        # Otherwise, return an error message:
-        else:
-            response['err_msg'] = f'Listing with id {listing_id}\
-                                    not found in database'
-            code = 400
-    # Not an admin:
+    # Otherwise, return an error message:
     else:
-        response['err_msg'] = 'Access Denied.'
-        code = 403
+        response['err_msg'] = f'Listing with id {listing_id}\
+                                not found in database'
+        code = 400
 
     return response, code
