e65f7d4af4c92aa316087aac091fe49c3cdee732
jventura1738 <justinv9001@aol.com>
2021-11-18 21:01:11 -0500

    Move listing GET routes to routes.py

--- api/admin.py
+++ api/admin.py
@@ -7,7 +7,7 @@ This module contains routes specifically for the admin.
 # Flask imports:
 from flask import Blueprint, request
 
-from .models import db, ListingsModel, ClientsModel
+from .models import db, ListingsModel
 from .constants import LISTING_STATUSES
 from .helpers import admin_session
 
@@ -27,90 +27,6 @@ def _admin_session():
         return {'err_msg': 'ACCESS DENIED.'}, 403
 
 
-# STATUSES: inactive, active, pending, rejected.
-@admin.route('/get-listings/<status>', methods=['GET'])
-def get_listings(status: str = 'all'):
-    """Route returns json payload of all <status> listings:
-
-    NOTE: must be in admin session.
-
-    JSON payload format:
-    {
-        'i': {
-            'client': client_name,
-            'listing': {
-                'id': listing.id,
-                'client_id': client_id,
-                'position': position,
-                'pos_responsibility': pos_responsibility,
-                'min_qualifications': min_qualifications,
-                'pref_qualifications': pref_qualifications,
-                'additional_info': additional_info,
-                'status': status,
-                'starred': starred,
-            }
-        }
-    }
-    """
-    response = dict()
-
-    # Handle possible listing statuses:
-    listings = []
-
-    # All statuses:
-    if status == 'all':
-        listings = ListingsModel.query.all()
-
-    # Query for one of the four statuses:
-    elif status in LISTING_STATUSES:
-        listings = ListingsModel.query.filter_by(status=status).all()
-
-    # Invalid listings request:
-    else:
-        response['err_msg'] = 'Invalid listings request.'
-        return response, 400
-
-    # Valid listing request:
-    if listings is not None:
-        code = 200
-        # For all pending listings, create a payload for each one:
-        for i, listing in enumerate(listings):
-
-            # Use foreign key to get client name via client_id:
-            temp = ClientsModel.query.filter_by(id=listing.client_id)\
-                .first()
-            client_name = temp.client_name
-
-            # Create the payload:
-            response[i] = {
-                'client': client_name,
-                'listing': listing.to_dict()
-            }
-
-    # No listings found.
-    else:
-        response['err_msg'] = 'No listings found.'
-        code = 200
-
-    return response, code
-
-  
-# Get a singular listing:
-@admin.route('/get-listing/<id>', methods=['GET'])
-def get_listing(id: int):
-    """Get a singular listing with the given id"""
-    response = dict()
-
-    if listing := ListingsModel.query.filter_by(id=id).first():
-        response['listing'] = listing.to_dict()
-        code = 200
-    else:
-        response['err_msg'] = f'Listing with id: {id} not found.'
-        code = 401
-
-    return response, code
-
-  
 # Route for changing a listings status.
 @admin.route('/set-status/<id>/<status>', methods=['PUT'])
 def action_on_listing(id: int, status: str):
