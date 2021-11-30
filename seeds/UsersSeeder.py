d8a23090dae1fc3eeac83438b19082daddac1cff
jventura1738 <justinv9001@aol.com>
2021-11-29 22:59:58 -0500

    CHANGE ADMIN PASSWORDS - PLEASE LOOK

--- seeds/UsersSeeder.py
+++ seeds/UsersSeeder.py
@@ -1,4 +1,4 @@
-from flask_seeder import Seeder, generator, Faker
+from flask_seeder import Seeder
 from api.models import db, UsersModel
 import hashlib  # Using for password hashing (SHA-256)
 
@@ -9,45 +9,31 @@ class UsersSeeder(Seeder):
     # run() will be called by Flask-Seeder
     def run(self):
         # Create a new Faker and tell it how to create User objects
-        faker = Faker(
-            cls=UsersModel,
-            init={
-                "username": generator.Name(),
-                "email": generator.Email(),
-                "password": hashlib.sha256("password".encode()).hexdigest(),
-            }
-        )
-
-        # Create 5 users
-        for user in faker.create(5):
-            print("Adding user: %s" % user)
-            db.session.add(user)
-        db.session.commit()
 
         admins = [
             {
-             'username': 'jventura3',
-             'email': 'jventura3@gulls.salisbury.edu',
-             'password': self.password_hash('joe momma'),
-             'is_admin': True
+                'username': 'jventura3',
+                'email': 'jventura3@gulls.salisbury.edu',
+                'password': self.password_hash('justinventura425'),
+                'is_admin': True
             },
             {
-             'username': 'jduncan5',
-             'email': 'jduncan5@gulls.salisbury.edu',
-             'password': self.password_hash('valorant'),
-             'is_admin': True
+                'username': 'jduncan5',
+                'email': 'jduncan5@gulls.salisbury.edu',
+                'password': self.password_hash('jacobduncan425'),
+                'is_admin': True
             },
             {
-             'username': 'bmason3',
-             'email': 'bmason3@gulls.salisbury.edu',
-             'password': self.password_hash('lapras'),
-             'is_admin': True
+                'username': 'bmason3',
+                'email': 'bmason3@gulls.salisbury.edu',
+                'password': self.password_hash('blainemason425'),
+                'is_admin': True
             },
             {
-             'username': 'jtanderson',
-             'email': 'jtanderson@gulls.salisbury.edu',
-             'password': self.password_hash('noob'),
-             'is_admin': True
+                'username': 'jtanderson',
+                'email': 'jtanderson@gulls.salisbury.edu',
+                'password': self.password_hash('joeanderson425'),
+                'is_admin': True
             }
         ]
         for admin in admins:
