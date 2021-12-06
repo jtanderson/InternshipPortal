bbab5972e6a441e26f351ce18a3f9f99290b8815
Meg Finnegan <mfinnegan101@gmail.com>
2021-12-05 19:34:48 -0500

    add grid to admin dashboard

--- src/components/AdminDashboard.vue
+++ src/components/AdminDashboard.vue
@@ -1,15 +1,55 @@
 <template>
+<<<<<<< Updated upstream
   <div class="bg-gray-100">
     <PendingListingModule />
   </div>
+=======
+  <section>
+    <div class="min-h-screen"> 
+      <div class="grid grid-cols-5 grid-rows-2 gap-3 p-5">
+        <div class="col-start-1 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">15 Total Listings</div> 
+        <div class="col-start-2 col-span-4 row-start-1 row-end-3 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 pb-20 rounded-lg">
+          <p>Positions Density</p>
+          
+
+        </div>
+        <div class="relative col-start-2 col-span-4 row-start-3 row-end-6 bg-gray-100 text-gray-500 text-lg font-bold text-center py-0 px-10 rounded-lg">
+            
+        <!-- <PendingListingModule/> -->
+
+        </div>
+        <div class="col-start-1 row-start-2 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Most Viewed Company</div>
+        <div class="col-start-1 row-start-3 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Software Engineer: Most Viewed Position</div>
+        <div class="col-start-1 row-start-4 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">20 Total Monthly Visits</div>
+        <div class="col-start-1 row-start-5 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">5 Pending Contact Requests</div>
+        
+      </div>
+    </div>
+  </section>
+>>>>>>> Stashed changes
 </template>
 
 <script>
+<<<<<<< Updated upstream
 import PendingListingModule from "./PendingListingModule.vue";
 export default {
   name: "AdminDashboard",
   components: {
     PendingListingModule,
   },
+=======
+// import AdminStats from "./AdminStats.vue";
+// import AdminCard from "./AdminCard.vue";
+// import PendingListingModule from "./PendingListingModule.vue";
+
+export default {
+  name: "AdminDashboard",
+
+  // components: {
+  //   AdminStats,
+  //   AdminCard,
+  //   PendingListingModule,
+  // }
+>>>>>>> Stashed changes
 };
 </script>
