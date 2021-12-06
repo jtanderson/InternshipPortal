2124612351669fc175b96ef9204498b96813bc15
Meg Finnegan <mfinnegan101@gmail.com>
2021-12-06 17:29:41 -0500

    add refactored admin dashboard

--- src/components/AdminDashboard.vue
+++ src/components/AdminDashboard.vue
@@ -1,22 +1,19 @@
 <template>
-<<<<<<< Updated upstream
-  <div class="bg-gray-100">
-    <PendingListingModule />
-  </div>
-=======
   <section>
-    <div class="min-h-screen"> 
-      <div class="grid grid-cols-5 grid-rows-2 gap-3 p-5">
+    <div class="container mx-auto">
+      <div class="min-h-screen"> 
+      <div class="grid grid-cols-2 md:grid-grid-cols-3 lg:grid-cols-5 grid-rows-2 gap-3 p-2">
         <div class="col-start-1 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">15 Total Listings</div> 
-        <div class="col-start-2 col-span-4 row-start-1 row-end-3 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 pb-20 rounded-lg">
+        <div class="col-start-2 col-span-4 row-start-1 row-end-3 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">
           <p>Positions Density</p>
-          
-
+          <div class="grid place-items-center pt-10">
+          <!-- <PieGraph :pieData="pieData"/>  -->
+          </div>
         </div>
-        <div class="relative col-start-2 col-span-4 row-start-3 row-end-6 bg-gray-100 text-gray-500 text-lg font-bold text-center py-0 px-10 rounded-lg">
-            
-        <!-- <PendingListingModule/> -->
-
+        <div class="relative col-start-2 col-span-4 row-start-3 row-end-6 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">
+          <p>Pending Listings</p>
+            <PendingListingModule/>
+         
         </div>
         <div class="col-start-1 row-start-2 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Most Viewed Company</div>
         <div class="col-start-1 row-start-3 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">Software Engineer: Most Viewed Position</div>
@@ -24,32 +21,31 @@
         <div class="col-start-1 row-start-5 bg-gray-100 text-gray-500 text-lg font-bold text-center p-10 rounded-lg">5 Pending Contact Requests</div>
         
       </div>
+      </div>
     </div>
   </section>
->>>>>>> Stashed changes
 </template>
 
 <script>
-<<<<<<< Updated upstream
-import PendingListingModule from "./PendingListingModule.vue";
+//  import PieGraph from "./PieGraph.vue";
+ import PendingListingModule from "./PendingListingModule.vue";
 export default {
   name: "AdminDashboard",
+   
+  // data:() => { 
+  //    return { 
+  //      pieData: [
+  //        {color: "#0B6487", value: 30}, 
+  //        {color: "#9D1F37", value: 40}, 
+  //        {color: "#F6931C", value: 30}
+
+  //      ]
+  //    };
+  //  },
+
   components: {
-    PendingListingModule,
-  },
-=======
-// import AdminStats from "./AdminStats.vue";
-// import AdminCard from "./AdminCard.vue";
-// import PendingListingModule from "./PendingListingModule.vue";
-
-export default {
-  name: "AdminDashboard",
-
-  // components: {
-  //   AdminStats,
-  //   AdminCard,
-  //   PendingListingModule,
-  // }
->>>>>>> Stashed changes
+    //  PieGraph,
+     PendingListingModule,
+   }
 };
 </script>
--- src/components/PendingListingModule.vue
+++ src/components/PendingListingModule.vue
@@ -1,13 +1,13 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div class="flex flex-col w-3/4 m-auto h-screen">
+  <div class="flex flex-col w-5/6 m-auto">
     <div class="mt-20">
       <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div
             class="
               shadow
-              overflow-hidden
+              overflow-hiddensm:px-6 lg:px-8
               border-b border-gray-200
               sm:rounded-lg
             "
