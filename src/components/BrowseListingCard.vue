5eaa6a65975f0ce2ae936dc45035507bb713987b
jacobduncan00 <jacobduncan00@yahoo.com>
2022-03-05 17:25:06 -0500

    Add filter, sort, input placeholder, grid, table view, etc

---
+++ src/components/BrowseListingCard.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="flex flex-wrap">
+    <div
+      v-for="listing in listings"
+      class="sm:flex-sm md:flex-md lg:flex-lg last:flex-el flex-nm m-4 shadow-lg rounded-xl bg-gray-100 cursor-pointer"
+      :key="listing[1].listing.id"
+    >
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">
+          {{ listing[1].listing.position }}
+        </div>
+        <p class="text-black font-semibold text-base">
+          Client:
+          <span class="text-gray-700 font-normal">{{ listing[1].client }}</span>
+        </p>
+        <p class="text-black font-semibold text-base">
+          Minimum Qualifications:
+          <span class="text-gray-700 font-normal">{{
+            listing[1].listing.min_qualifications
+          }}</span>
+        </p>
+        <p class="text-black font-semibold text-base">
+          Preferred Qualifications:
+          <span class="text-gray-700 font-normal">{{
+            listing[1].listing.pref_qualifications
+          }}</span>
+        </p>
+        <p class="text-black font-semibold text-base">
+          Additional Info:
+          <span class="text-gray-700 font-normal">{{
+            listing[1].listing.additional_info
+          }}</span>
+        </p>
+        <p class="text-black font-semibold text-base">
+          Responsibilities:
+          <span class="text-gray-700 font-normal">{{
+            listing[1].listing.pos_responsibility
+          }}</span>
+        </p>
+        <p class="text-black font-semibold text-base">
+          Duration:
+          <span class="text-gray-700 font-normal"
+            >{{
+              listing[1].listing.duration != null
+                ? listing[1].listing.duration + " weeks"
+                : ""
+            }}
+          </span>
+        </p>
+        <p class="text-black font-semibold text-base">
+          Open Date:
+          <span class="text-gray-700 font-normal">{{
+            listing[1].listing.app_open
+          }}</span>
+        </p>
+        <p class="text-black font-semibold text-base">
+          Close Date:
+          <span class="text-gray-700 font-normal">{{
+            listing[1].listing.app_close
+          }}</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BrowseListingCard",
+  props: ["listings"],
+  setup() {
+    function toListingPage(listing_id) {
+      window.location.href = `/admin/listing/${listing_id}`;
+    }
+    return {
+      toListingPage,
+    };
+  },
+};
+</script>
