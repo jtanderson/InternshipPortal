3d6c4988a8b5758f36ba8c22e9c7e77d7f04d7d4
Meg Finnegan <mfinnegan101@gmail.com>
2022-04-11 23:15:50 -0400

    add changes to ui

--- src/components/BrowseListingCard.vue
+++ src/components/BrowseListingCard.vue
@@ -1,43 +1,45 @@
 <template>
-  <div class="flex flex-wrap">
+  <div class="flex flex-wrap mx-auto my-auto">
     <div
       v-for="listing in listings"
-      class="sm:flex-sm md:flex-md lg:flex-lg last:flex-el flex-nm m-4 shadow-lg rounded-xl bg-gray-100 cursor-pointer"
+      class="sm:flex-sm md:flex-md lg:flex-lg last:flex-el flex-nm m-4 shadow-xl rounded-xl bg-white cursor-pointer"
       :key="listing[1].listing.id"
     >
       <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">
+        <div class="text-red-700 font-bold text-xl mb-2">
           {{ listing[1].listing.position }}
         </div>
-        <p class="text-black font-semibold text-base">
+        <p v-if="listing[1].listing.client != null" class="text-gray-900 font-semibold text-base">
           Client:
           <span class="text-gray-700 font-normal">{{ listing[1].client }}</span>
         </p>
-        <p class="text-black font-semibold text-base">
+        <p class="text-gray-900 font-semibold text-base">
           Minimum Qualifications:
           <span class="text-gray-700 font-normal">{{
             listing[1].listing.min_qualifications
           }}</span>
         </p>
-        <p class="text-black font-semibold text-base">
+        <p v-if="listing[1].listing.pref_qualifications != null" class="text-gray-900 font-semibold text-base">
           Preferred Qualifications:
           <span class="text-gray-700 font-normal">{{
             listing[1].listing.pref_qualifications
           }}</span>
         </p>
-        <p class="text-black font-semibold text-base">
+        <p v-if="listing[1].listing.additional_info != null" class="text-gray-900 font-semibold text-base">
+          
           Additional Info:
           <span class="text-gray-700 font-normal">{{
             listing[1].listing.additional_info
           }}</span>
+          
         </p>
-        <p class="text-black font-semibold text-base">
+        <p v-if="listing[1].listing.pos_responsibility != null" class="text-gray-900 font-semibold text-base">
           Responsibilities:
           <span class="text-gray-700 font-normal">{{
             listing[1].listing.pos_responsibility
           }}</span>
         </p>
-        <p class="text-black font-semibold text-base">
+        <p v-if="listing[1].listing.duration != null" class="text-gray-900 font-semibold text-base">
           Duration:
           <span class="text-gray-700 font-normal"
             >{{
@@ -47,36 +49,44 @@
             }}
           </span>
         </p>
-        <p class="text-black font-semibold text-base">
+        <p class="text-gray-900 font-semibold text-base">
           Open Date:
           <span class="text-gray-700 font-normal">{{
             listing[1].listing.app_open
           }}</span>
         </p>
-        <p class="text-black font-semibold text-base">
+        <p class="text-gray-900 font-semibold text-base">
           Close Date:
           <span class="text-gray-700 font-normal">{{
             listing[1].listing.app_close
           }}</span>
         </p>
-        <div class="text-black font-semibold text-base">
+        <div class="text-gray-900 font-semibold text-base">
           Tags:
           <div v-if="listing[1].tags != null && listing[1].tags.length > 0">
             <div v-for="tag in listing[1].tags" :key="tag">
-              <span class="text-gray-700 font-normal"> - {{ tag }} </span>
+              <span
+              class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-200 text-gray-600"
+              >
+                {{ tag }}
+              </span>
             </div>
           </div>
           <div v-else>
             <span class="text-gray-700 font-normal"> No Tags </span>
           </div>
         </div>
-        <div class="text-black font-semibold text-base">
+        <div class="text-gray-900 font-semibold text-base">
           SU Courses:
           <div
             v-if="listing[1].courses != null && listing[1].courses.length > 0"
           >
             <div v-for="course in listing[1].courses" :key="course">
-              <span class="text-gray-700 font-normal"> - {{ course }} </span>
+              <span
+              class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-200 text-gray-600"
+              >
+                {{ course }}
+              </span>
             </div>
           </div>
           <div v-else>
